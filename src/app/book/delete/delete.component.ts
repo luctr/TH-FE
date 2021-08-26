4571999a952538af710061ff3e4dde55363a1eea
luctr <dinh.luc.3192@gmail.com>
2021-08-26 10:34:06 +0700

    Add

--- src/app/book/delete/delete.component.ts
+++ src/app/book/delete/delete.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 import {Book} from "../../model/book";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import {BookServiceService} from "../service/book-service.service";
 
 @Component({
   selector: 'app-delete',
@@ -8,6 +10,8 @@ import {Book} from "../../model/book";
   styleUrls: ['./delete.component.scss']
 })
 export class DeleteComponent implements OnInit {
+
+  id: any;
 
   // @ts-ignore
   bookForm: FormGroup
@@ -18,9 +22,35 @@ export class DeleteComponent implements OnInit {
     author: "",
     description: "",
   }
-  constructor() { }
+
+  constructor(
+    private bookService: BookServiceService,
+    private activeRouter: ActivatedRoute,
+    private router: Router) {
+    this.activeRouter.paramMap.subscribe((paraMap: ParamMap) => {
+      this.id = paraMap.get('id')
+    })
+  }
 
   ngOnInit(): void {
+    this.getBook(this.id)
+  }
+
+  getBook(id:number){
+    this.bookService.getBookById(id).subscribe(data=>{
+      this.bookForm = new FormGroup({
+        id:new FormControl(data.id),
+        title: new FormControl(data.title),
+        author: new FormControl(data.author),
+        description: new FormControl(data.description),
+      })
+    })
+  }
+  deleteBook(id: number){
+    console.log(id)
+    this.bookService.deleteBook(id).subscribe(data =>{
+      this.router.navigate([''])
+    })
   }
 
 }
